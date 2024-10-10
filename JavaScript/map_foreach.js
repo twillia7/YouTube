977c647b8bf4dd5c9ec5c07c0795f3988ed0581b
twillia7 <taylorwilliamsdev@icloud.com>
2024-10-09 21:31:40 -0600

    save examples

--- JavaScript/map_foreach.js
+++ JavaScript/map_foreach.js
@@ -1,2 +1,38 @@
 const numbers = [1, 2, 3, 4, 5];
-const objects = [{ name: 'John' }, { name: 'Jane' }, { name: 'Jack' }, { name: 'Mark' }];
+const objects = [{ name: 'John', age: 20 }, { name: 'Jim', age: 25 }, { name: 'Jack', age: 22 }, { name: 'Mark', age: 28 }];
+
+// const objectWithLength = objects.map((object) => {
+//   object.length = object.name.length;
+//   return object;
+// });
+
+// const doubleNumbers = numbers.map((number) => {
+//   return number * 2;
+// });
+
+// const nameList = objects.map((object) => {
+//   return object.name;
+// });
+
+// numbers.filter((number) => {
+//   return number % 2 === 0;
+// }).forEach((number) => {
+//   console.log(number);
+// });
+
+// objects.forEach((object) => {
+//   console.log(`${object.name} is ${object.age} years old.`);
+// });
+
+// let count = 0;
+// objects.forEach(obj => {
+//   if (obj.name.startsWith('J')) {
+//     count++;
+//   }
+// });
+// console.log(`There are ${count} names that start with 'J'.`);
+
+// const messages = objects.map((object) => {
+//   return `${object.name} is ${object.age} years old.`;
+// });
+// console.log(messages);
